21427e2b488dcb5ba5f424a3c9fbaf0a529226d0
yaluvv <yaluvv@yandex.ru>
2022-07-17 16:34:38 +0300

    added files

---
+++ js/app.js
@@ -0,0 +1,62 @@
+const slides = document.querySelectorAll('.slider__slide')
+const dots = document.querySelectorAll('.slider__dot')
+const sliderNext = document.querySelector('.slider__next')
+const burgerOpen = document.querySelector('.menu-open')
+const burgerClose = document.querySelector('.menu-close')
+const navMenu = document.querySelector('.header__nav')
+
+let index = 0
+
+
+const activeSlide = n => {
+    for (slide of slides) {
+        slide.classList.remove('slider__slide_active')
+    }
+    slides[n].classList.add('slider__slide_active')
+}
+const activeDot = n => {
+    for (dot of dots) {
+        dot.classList.remove('slider__dot_active')
+    }
+    dots[n].classList.add('slider__dot_active')
+}
+const prepareCurrentSlide = ind => {
+    activeSlide(index)
+    activeDot(index)
+}
+
+const nextSlide = () => {
+    if (index == slides.length - 1) {
+        index = 0
+        prepareCurrentSlide(index)
+    } else {
+        index++
+        prepareCurrentSlide(index)
+    }
+}
+const prevSlide = () => {
+    if (index == 0) {
+        index == slides.length - 1
+        prepareCurrentSlide(index)
+    } else {
+        index--
+        prepareCurrentSlide(index)
+    }
+}
+dots.forEach((item, indexDot) => {
+    item.addEventListener('click', () => {
+        index = indexDot
+        prepareCurrentSlide(index)
+    })
+})
+
+setInterval(nextSlide, 5000)
+
+burgerOpen.addEventListener('click', () => {
+    navMenu.classList.add('header__nav--active')
+    burgerClose.classList.add('menu-close--active')
+})
+burgerClose.addEventListener('click', () => {
+    navMenu.classList.remove('header__nav--active')
+    burgerClose.classList.remove('menu-close--active')
+})
